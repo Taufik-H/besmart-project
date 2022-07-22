e7ad15102c8d83de640abb4bdb8fbf6eba012c02
Taufik-H <taufikrpl1@gmail.com>
2022-07-22 20:21:46 +0700

    header responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,16 +7,22 @@ export const Hero = () => {
         <div className="hidden lg:block lg:absolute lg:top-0 opacity-80 -z-10">
             <div className="relative ">
            <img src={Blob} alt="blob" className="bg-cover bg-center -mt-24" />
-           
+
             </div>
             
+        </div>
+        
+        <div className="block lg:hidden flex justify-center h-full overflow-hidden lg:w-2/3 xl:w-1/2 lg:absolute lg:justify-start lg:bottom-0 lg:right-0 lg:items-end">
+            <div className="relative">
+             <img src={Heroimage}alt="herobg" className=""/>
+            </div>
         </div>
             <div className="relative px-4 pt-16 mx-auto lg:py-32 md:px-8 xl:px-20 sm:max-w-xl md:max-w-full">
         <div className="max-w-xl mx-auto lg:max-w-screen-xl">
           <div className="mb-16 lg:max-w-lg lg:mb-0">
             <div className="max-w-xl mb-16">
               <div>
-                <p className="inline-block p-2 border border-red-400 mb-4 text-xs font-semibold tracking-wider text-red-500 uppercase rounded-full bg-red-100">
+                <p className="inline-block p-1 border border-red-400 mb-4 text-xs lg:text-sm font-semibold tracking-wider text-red-500 uppercase rounded-full bg-red-100">
                 KONSULTASI KESEHATAN
                 </p>
               </div>
@@ -47,7 +53,7 @@ export const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="flex justify-center h-full overflow-hidden lg:w-2/3 xl:w-1/2 lg:absolute lg:justify-start lg:bottom-0 lg:right-0 lg:items-end">
+        <div className="hidden lg:flex justify-center h-full overflow-hidden lg:w-2/3 xl:w-1/2 lg:absolute lg:justify-start lg:bottom-0 lg:right-0 lg:items-end">
             <div className="relative -top-20">
              <img src={Heroimage}alt="herobg" className=""/>
             </div>
