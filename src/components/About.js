b060de4ea39875f45ea790e5babcef9b6d193a36
Taufik-H <taufikrpl1@gmail.com>
2022-07-23 08:10:26 +0700

    responsifity about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,67 +3,52 @@ import Hati from "./images/1.png"
 export const About = () => {
     return (
         <>
-        
-        <div className="">
+
             <div className="">
-                <div className="text-xs text-red-400 font-bold uppercase tracking-wide px-12">
-                    about
-                </div>
-                <div className="text-3xl text-gray-700 font-bold px-12">
-                    Apa Itu Hepatitis?
-                </div>
-                <div className="grid grid-cols-1 lg:grid-cols-3  ">
-                    <div>                
-                        <div className="mt-5 text-gray-700 ml-12">
-                        Hepatitis adalah peradangan pada hati atau liver. Kondisi ini 
-                        bisa disebabkan oleh berbagai hal, mulai dari infeksi virus, kebiasaan 
-                        mengonsumsi alkohol, penggunaan obat-obatan tertentu, penyakit autoimun,
-                        dan infeksi cacing hati. Jika disebabkan oleh infeksi virus, hepatitis bisa
-                        menular ke orang lain
-                        </div> 
-                        <div className="flex mt-7  mb-7 w-48 justify-center py-2 ml-12 bg-red-200 rounded-lg">
-                            <span className="text-red-600 font-semibold text-md mr-2">Lebih Banyak</span>
-                            <div className="ml-1 text-red-600">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
-                            </svg>
+                <div className="text-center lg:text-left">
+                    <div className="text-xs text-red-400 font-bold uppercase tracking-wide px-12">
+                        about
+                    </div>
+                    <div className="text-3xl text-gray-700 font-bold px-12">
+                        Apa Itu Hepatitis?
+                    </div>
+                    <div className="grid grid-cols-1 lg:grid-cols-3  ">
+                        <div>
+                            <div className=" text-center lg:text-left mt-5 text-gray-700 lg:ml-12 px-12 lg:px-0">
+                                Hepatitis adalah peradangan pada hati atau liver. Kondisi ini
+                                bisa disebabkan oleh berbagai hal, mulai dari infeksi virus, kebiasaan
+                                mengonsumsi alkohol, penggunaan obat-obatan tertentu, penyakit autoimun,
+                                dan infeksi cacing hati. Jika disebabkan oleh infeksi virus, hepatitis bisa
+                                menular ke orang lain
                             </div>
+                            <div className="flex mt-7 w-full text-center lg:mb-7 lg:w-48 justify-center lg:py-2  lg:ml-12 px-12  lg:bg-red-200 rounded-lg">
+                                <span className="text-red-600 font-semibold text-md mr-2">Lebih Banyak</span>
+                                <div className="ml-1 text-red-600">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-span-2 ">
+                            <div className="p-5 md:px-12 lg:p-0">
+                                 <div className="mt-5  py-10 h-full bg-blue-500 rounded-full md:rounded-xl md:-mt-10 lg:rounded-l-full lg:w-3/4 lg:float-right">
+                                <div className="flex justify-center  ">
+                                    <img src={Hati} alt="Hati" className="w-3/6 mx-auto my-auto animate-pulse-slow " />
+                                    <div className=" ">
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+                           
+
                         </div>
                     </div>
-                  <div className="col-span-2 ">
-                        <div className="absolte float-right w-8/12  -mt-32 ">
-                            <div className="relative w-8/12">
-                            <svg width="600" height="532" viewBox="0 0 854 532" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M0 252C0 112.824 112.824 0 252 0H1201V532H252C112.824 532 0 419.176 0 280V252Z" fill="#84BBFC"/>
-                            </svg>
-                            </div>
-                        </div>
-                        <div className="absolute ">
-                        <div className="relative w-4/12 -right-96 ">
-                        <img src={Hati} alt="Hati" className="ml-20 animate-bounce-slow mt-10"/>
-                        <div className="relative -right-24 -top-14 ">
-                        <svg width="218" height="118" viewBox="0 0 318 118" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g filter="url(#filter0_f_6_19)">
-                        <ellipse cx="159" cy="59" rx="120" ry="20" fill="#9D4242" fill-opacity="0.44"/>
-                        </g>
-                        <defs>
-                        <filter id="filter0_f_6_19" x="0" y="0" width="318" height="118" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-                        <feGaussianBlur stdDeviation="19.5" result="effect1_foregroundBlur_6_19"/>
-                        </filter>
-                        </defs>
-                        </svg>
-                        </div>
-                        </div>
-                        </div>
-                       
-                    </div> 
+
                 </div>
-                 
             </div>
-        </div>
         </>
     );
-  };
-  export default About
+};
+export default About
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,41 +2,41 @@ import Heroimage from "./images/herobg.png"
 import Blob from "./images/blob.png"
 import Bgicon from "./images/bg.png"
 export const Hero = () => {
-    return (
-        <>
-        <div className="hidden lg:block lg:absolute lg:top-0 opacity-80 -z-10">
-            <div className="relative ">
-           <img src={Blob} alt="blob" className="bg-cover bg-center -mt-24" />
+  return (
+    <>
+      <div className="hidden lg:block lg:absolute lg:top-0 opacity-80 -z-10">
+        <div className="relative ">
+          <img src={Blob} alt="blob" className="bg-cover bg-center -mt-24" />
 
-            </div>
-            
         </div>
-        
-        <div className="block lg:hidden flex justify-center h-full overflow-hidden lg:w-2/3 xl:w-1/2 lg:absolute lg:justify-start lg:bottom-0 lg:right-0 lg:items-end">
-            <div className="relative">
-             <img src={Heroimage}alt="herobg" className=""/>
-            </div>
+
+      </div>
+
+      <div className=" lg:hidden flex justify-center h-full overflow-hidden lg:w-2/3 xl:w-1/2 lg:absolute lg:justify-start lg:bottom-0 lg:right-0 lg:items-end">
+        <div className="relative">
+          <img src={Heroimage} alt="herobg" className="" />
         </div>
-            <div className="relative px-4 pt-16 mx-auto lg:py-32 md:px-8 xl:px-20 sm:max-w-xl md:max-w-full">
+      </div>
+      <div className="relative px-4 pt-16 mx-auto lg:py-32 md:px-8 xl:px-20 sm:max-w-xl md:max-w-full">
         <div className="max-w-xl mx-auto lg:max-w-screen-xl">
           <div className="mb-16 lg:max-w-lg lg:mb-0">
             <div className="max-w-xl mb-16">
               <div>
                 <p className="inline-block p-1 border border-red-400 mb-4 text-xs lg:text-sm font-semibold tracking-wider text-red-500 uppercase rounded-full bg-red-100">
-                KONSULTASI KESEHATAN
+                  KONSULTASI KESEHATAN
                 </p>
               </div>
               <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-700 sm:text-4xl sm:leading-none">
-                 Kenali gejala <span className="text-red-400 font-bold mr-2"> Hepatitis</span>
+                Kenali gejala <span className="text-red-400 font-bold mr-2"> Hepatitis</span>
                 <br className="hidden md:block" />
-                    {''}
+                {''}
                 <span className="inline-block text-deep-purple-accent-400 mt-6">
-                 sejak dini!
+                  sejak dini!
                 </span>
               </h2>
               <p className="text-base text-gray-700 md:text-lg">
-       
-               Mengenal lebih dalam tentang penyebab, risiko, gejala, pengobatan,
+
+                Mengenal lebih dalam tentang penyebab, risiko, gejala, pengobatan,
                 pencegahan, dan berita terbaru tentang penyakit hepatitis.
               </p>
             </div>
@@ -46,7 +46,7 @@ export const Hero = () => {
                 className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded-full shadow-md bg-red-400 hover:bg-red-700 focus:shadow-outline focus:outline-none"
               >
                 <span className="mr-2">Mulai Sekarang</span>  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                     <path stroke-linecap="round" stroke-linejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
                 </svg>
               </a>
 
@@ -54,13 +54,13 @@ export const Hero = () => {
           </div>
         </div>
         <div className="hidden lg:flex justify-center h-full overflow-hidden lg:w-2/3 xl:w-1/2 lg:absolute lg:justify-start lg:bottom-0 lg:right-0 lg:items-end">
-            <div className="relative -top-20">
-             <img src={Heroimage}alt="herobg" className=""/>
-            </div>
+          <div className="relative -top-20">
+            <img src={Heroimage} alt="herobg" className="" />
+          </div>
         </div>
       </div>
-        </>
-  
-    );
-  };
+    </>
+
+  );
+};
 export default Hero
