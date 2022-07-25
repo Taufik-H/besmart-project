63106538ec3b85764a5015bf1654efcfd8a135ea
Taufik-H <taufikrpl1@gmail.com>
2022-07-25 18:00:51 +0700

    fixing responsivity

--- src/components/Subs.js
+++ src/components/Subs.js
@@ -24,7 +24,7 @@ export const Subs = () => {
                         <div class="flex items-center">
                             <div class="mx-auto md:mx-0">
                                 <h3 class="text-4xl font-bold text-white">Subscribe</h3>
-                                <p class="mt-2 max-w-[20rem] text-lg text-white/80">Join our weekly digest. You'll also receive some of our best posts today.</p>
+                                <p class="mt-2 max-w-[20rem] text-lg text-white/80">Bergabunglah dengan intisari mingguan kami. Anda juga akan menerima beberapa posting terbaik kami hari ini.</p>
                                 <form action="" class="mt-4 flex flex-col">
                                     <input type="email" name="email" id="email" placeholder="Enter your email" class="w-full rounded border border-white/50 bg-transparent px-3 py-2 text-white placeholder:text-white/50 md:max-w-[18rem]" />
                                     <button type="submit" class="mt-4 w-full max-w-[14rem] rounded bg-white/30 px-14 py-2 text-center text-white">Subscribe</button>
