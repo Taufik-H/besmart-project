ec625a3153cfff47fb9ee4c57aa273acd2f57463
Taufik-H <taufikrpl1@gmail.com>
2022-07-25 17:58:46 +0700

    fixing responsivity

--- src/components/Pencegahan.js
+++ src/components/Pencegahan.js
@@ -2,7 +2,7 @@ export const Pencegahan = () => {
     return (
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <h5 className="mb-2 mb-8 text-4xl font-extrabold leading-none md:pl-2">
-          Penanganan dan 
+          Penanganan dan {' '}
           <br className="hidden md:block" />
           Pengobatan{' '}
           <span className="inline-block text-red-400">
